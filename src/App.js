301d3f6481e242a31f696f47aa64f425110d7b93
Matheus Lima <mlbp@cin.ufpe.br>
2022-12-22 12:25:36 -0300

    Adding title and search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { SearchBarContainer, TitleContainer } from "./App.styles";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <TitleContainer>Backlog estimator</TitleContainer>
+
+      <SearchBarContainer placeholder="Search for a game..." cols="57" rows="1" />
+
     </div>
   );
 }
---
+++ src/App.styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const TitleContainer = styled.h1`
+  color: white;
+  text-align: center;
+  font-size: 60px;
+  font-family: "Hahmlet";
+  margin-top: 150px;
+`;
+
+export const SearchBarContainer = styled.textarea`
+  display: block;
+  margin-inline: auto;
+  resize: none;
+  background-color: #5A5A5A;
+  border: 7px solid #A1A1A1;
+  border-radius: 40px;
+  padding: 12px;
+  overflow-wrap: normal;
+  overflow: hidden;
+  max-height: 15px;
+`;
